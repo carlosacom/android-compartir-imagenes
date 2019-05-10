b44b4083aad4e30e9d59f7ff429f422f8cfed77d
Carlos Andres Perez Combariza <carlosaperez1997@gmail.com>
2019-05-09 21:25:54 -0500

    consumir firebase para traer las imagenes

--- app/src/main/java/com/example/helloword/NetworkFragment.java
+++ app/src/main/java/com/example/helloword/NetworkFragment.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -16,10 +23,13 @@ import java.util.ArrayList;
  * A simple {@link Fragment} subclass.
  */
 public class NetworkFragment extends Fragment {
+    private static final String TAG = "NetworkFragment" ;
     PublicationAdapter publicationAdapter;
-
+    FirebaseDatabase firebaseDatabase;
+    RecyclerView publicationRecycler;
     public NetworkFragment() {
         // Required empty public constructor
+        this.firebaseDatabase = FirebaseDatabase.getInstance();
     }
 
 
@@ -28,12 +38,12 @@ public class NetworkFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_network, container, false);
-        RecyclerView publication = (RecyclerView)view.findViewById(R.id.recyclerContainer);
+        this.publicationRecycler = (RecyclerView)view.findViewById(R.id.recyclerContainer);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        publication.setLayoutManager(linearLayoutManager);
-        publicationAdapter = new PublicationAdapter(getNetwork() ,R.layout.card,getActivity());
-        publication.setAdapter(publicationAdapter);
+        this.publicationRecycler.setLayoutManager(linearLayoutManager);
+        this.publicationAdapter = new PublicationAdapter(this.getNetworkCloud() ,R.layout.card,getActivity());
+        this.publicationRecycler.setAdapter(publicationAdapter);
         return view;
     }
     public ArrayList getNetwork() {
@@ -56,6 +66,36 @@ public class NetworkFragment extends Fragment {
         publications.add(new Plublication("1","paisaje","https://algo.tv/includes/Images/Algo2018_logo_large.png", 4));
         publications.add(new Plublication("1","paisaje","https://algo.tv/includes/Images/Algo2018_logo_large.png", 4));
         publications.add(new Plublication("1","paisaje","https://algo.tv/includes/Images/Algo2018_logo_large.png", 4));
+        return publications;
+    }
+    public ArrayList getNetworkCloud() {
+        DatabaseReference myRef = this.firebaseDatabase.getReference();
+        final ArrayList<Plublication> publications = new ArrayList();
+        // Read from the database
+        myRef.child("reportes").addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                publications.clear();
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot data:dataSnapshot.getChildren()) {
+                        Plublication publication = data.getValue(Plublication.class);
+                        publications.add(publication);
+                        Log.d(TAG, "Value is: ");
+                        publicationAdapter.notifyDataSetChanged();
+                    }
+                }
+                // publications.add(dataSnapshot.getValue(Plublication.class));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
         return publications;
     }
 }
